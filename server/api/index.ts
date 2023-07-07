7a7103adf9d406f8ced38d6fea5b470eea5b5a73
Harsh Patel <hipclem92@gmail.com>
2023-07-07 20:38:51 +0530

    master server side cors related changes

--- server/api/index.ts
+++ server/api/index.ts
@@ -2,20 +2,14 @@ import express from "express";
 import homeRouter from "./routes/home";
 import pkg from "body-parser";
 import errorHandler from "./exception/ErrorHandler";
-import cors from "cors";
 const { json } = pkg;
 const app = express();
 const port = 8000;
 
-app.use(
-  cors({
-    origin: ["*"],
-  })
-);
 app.use(json());
 app.use((req: any, res: any, next: any) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With, Content-Type, Accept"
